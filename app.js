950a5d578bfacaf522a640ed393cf28464077de5
ayziao <ayziao+git@gmail.com>
2015-02-25 08:07:35 +0900

    HTTPステータスコード

--- app.js
+++ app.js
@@ -6,9 +6,33 @@ var config = require('./config.json');
 var port = config.port || 80;
 var user = config.user || 'node';
 
+var httpStatus = {
+	200 : 'OK',
+	403 : 'Forbidden',
+	404 : 'NotFound',
+}
+
 http.createServer(function (request, response) {
-	response.writeHead(200, {'Content-Type': 'text/plain;charset=UTF-8'});
-	response.end('ハロー');
+
+	statusCode = 200;
+	requestTime = new Date();
+
+	if (request.method == 'GET') {
+		if (request.url == '/') {
+			response.writeHead(200, {'Content-Type': 'text/plain;charset=UTF-8'});
+			response.end('ハロー');
+		} else {
+			statusCode = 404;
+			response.writeHead(statusCode, {'Content-Type': 'text/plain;charset=UTF-8'});
+			response.end(statusCode + ' ' + httpStatus[statusCode]);
+		}
+	} else {
+		statusCode = 403;
+		response.writeHead(statusCode, {'Content-Type': 'text/plain;charset=UTF-8'});
+		response.end(statusCode + ' ' + httpStatus[statusCode]);
+	}
+	console.log(requestTime + ' ' + request.url + ' ' + statusCode + ' ' + httpStatus[statusCode]);
+
 }).listen(port, function () {
     console.log('ポート' + port + 'でHTTP接続待ち受け開始');
     if(port < 1025){
@@ -16,3 +40,5 @@ http.createServer(function (request, response) {
 	    process.setuid(user);
     }
 });
+
+
