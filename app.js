bcc936b20c1e59d3cba6f379228a2d58dd560d53
ayziao <ayziao+git@gmail.com>
2015-02-25 07:10:52 +0900

    ポート80で実行するとき対応

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ var http = require('http');
 
 var config = require('./config.json');
 var port = config.port || 80;
+var user = config.user || 'node';
 
 http.createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/plain;charset=UTF-8'});
 	response.end('ハロー');
 }).listen(port, function () {
     console.log('ポート' + port + 'でHTTP接続待ち受け開始');
+    if(port < 1025){
+	    console.log('実行ユーザを' + user + 'に変更');
+	    process.setuid(user);
+    }
 });
