a030708e9799d6077a5b01b75b3c0a7ca07d1967
ayziao <ayziao+git@gmail.com>
2015-02-24 11:43:16 +0900

    HTTPサーバ

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('ハロー');
+var http       = require('http');
+
+var port = 8080;
+var hostname = '127.0.0.1';
+
+http.createServer(function (request, response) {
+	response.writeHead(200, {
+		'Content-Type': 'text/plain;charset=UTF-8' ,
+	});
+	response.end('ハロー');
+
+}).listen(port, hostname, function () {
+    console.log('listen');
+});
