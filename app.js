f008dda2b1e7ac2a057d6a4390c3cc967996313b
ayziao <ayziao+git@gmail.com>
2015-03-23 15:03:07 +0900

    コメント整形

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-//アプリケーション
-//httpサーバ起動するだけ
+/**
+ * アプリケーション
+ * 
+ * httpサーバ起動するだけ
+ */
 
-//設定
+
+/**
+ * 設定
+ */
 var config    = require('./config.json').http;
 var port      = config.port || 80;
 var user      = config.user || 'node';
 
+
+/**
+ * モジュール
+ */
 //コアモジュール
 var http = require('http');
 
@@ -14,7 +24,10 @@ var http = require('http');
 //自作モジュール
 var nsweb   = require('./nsweb');
 
-//HTTPサーバ起動
+
+/**
+ * HTTPサーバ起動
+ */
 http.createServer(nsweb).listen(port, function () {
     console.log('ポート' + port + 'でHTTP接続待ち受け開始 ' + (new Date()).toISOString());
     if(port < 1025 && user != 'root'){
