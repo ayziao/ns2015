86a9ef46bffd6e32fda7d1969b2f24cac31f4c76
ayziao <ayziao+git@gmail.com>
2015-04-07 11:51:34 +0900

    コード整形

--- app.js
+++ app.js
@@ -10,9 +10,9 @@
 /**
  * 設定
  */
-var config    = require('./config.json').http;
-var port      = config.port || 80;
-var user      = config.user || 'node';
+var config = require('./config.json').http;
+var port   = config.port || process.env.PORT || 8080;
+var user   = config.user || 'node';
 
 
 /**
@@ -25,15 +25,15 @@ var http = require('http');
 
 
 //自作モジュール
-var nsweb   = require('./nsweb');
+var nsweb = require('./nsweb');
 
 /**
  * HTTPサーバ起動
  */
 http.createServer(nsweb).listen(port, function () {
-    console.log('ポート' + port + 'でHTTP接続待ち受け開始 ' + (new Date()).toISOString());
-    if(port < 1025 && user != 'root'){
-	    process.setuid(user);
-	    console.log('実行ユーザを' + user + 'に変更');
-    }
+	console.log('ポート' + port + 'でHTTP接続待ち受け開始 ' + (new Date()).toISOString());
+	if (port < 1025 && user != 'root') {
+		process.setuid(user);
+		console.log('実行ユーザを' + user + 'に変更');
+	}
 });
