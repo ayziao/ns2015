a5b77c25e23d35200eb83d566cf6b1026cae924d
ayziao <ayziao+git@gmail.com>
2015-04-28 06:44:29 +0900

    リファクタリング:ESLint

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* global require, process */
-'use strict';
+"use strict";
 
 /*
  * node.jsアプリケーション
@@ -8,33 +8,33 @@
  */
 
 //コアモジュール
-var http = require('http');
+var http = require("http");
 
 //Node Package Manager
 
 //自作モジュール
-var nsweb = require('./lib/nsweb');
+var nsweb = require("./lib/nsweb");
 
 
 /*
  * 設定
  */
-var config = require('./config/config.json').http;
+var config = require("./config/config.json").http;
 var port   = process.env.PORT || process.argv[2] || config.port || 8080; //PENDING コマンドライン引数ちゃんと考える
-var user   = config.user || 'node';
+var user   = config.user || "node";
 
 
 /*
  * HTTPサーバ起動
  */
 http.createServer(nsweb).listen(port, function () {
-	console.log('ポート' + port + 'でHTTP接続待ち受け開始 ' + (new Date()).toISOString());
-	if (port < 1025 && user !== 'root') {
+	console.log("ポート" + port + "でHTTP接続待ち受け開始 " + (new Date()).toISOString());
+	if (port < 1025 && user !== "root") {
 		process.setuid(user);
-		console.log('実行ユーザを' + user + 'に変更');
+		console.log("実行ユーザを" + user + "に変更");
 	}
 });
 
 //DEBUG あとで消す
-console.log({env:process.env});
-console.log('args: ', process.argv);
+console.log({env: process.env});
+console.log("args: ", process.argv);
